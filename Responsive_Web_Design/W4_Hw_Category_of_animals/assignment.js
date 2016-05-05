82017e612c3b448331738a2e1c194b29503e2d27
YongYuH <larry830326@hotmail.com>
2016-05-06 02:21:18 +0800

    modify button function and add a new button
    Each button used to display whole category of animals. Now it is
    modified to display only one animal. The function of new button is to
    show all the animals.

--- Responsive_Web_Design/W4_Hw_Category_of_animals/assignment.js
+++ Responsive_Web_Design/W4_Hw_Category_of_animals/assignment.js
@@ -8,20 +8,36 @@ $(document).ready(function(){
   var default_source = $("#default-template").html();
   var default_template = Handlebars.compile(default_source);
   var html =default_template(animals_data);
-  $('#animals').html(html);
+  $('#category').html(html);
+
+  var category_source = $("#category-template").html();
+  var category_template = Handlebars.compile(category_source);
 
   var animals_source = $("#animals-template").html();
   var animals_template = Handlebars.compile(animals_source);
-  // var html =animals_template(animals_data);
-  // $('#animals').html(html);
+
+  $("a.dropdown-toggle").click(function(){
+    var category_index = this.id;
+    var specific_category = animals_data.category[category_index];
+    var html = category_template(specific_category);
+    $('#category').html(html);
+    $('#animals').empty();
+  });
 
   $("li.animal_btn").click(function(){
     var category_index = this.id.split("-")[0];
     var animals_index = this.id.split("-")[1];
-    var specific_category = animals_data.category[category_index];
-    var specific_content = specific_category.animals[animals_index];
-    var html = animals_template(specific_category);
+    var specific_content = animals_data.category[category_index].animals[animals_index];
+    var html = animals_template(specific_content);
     $('#animals').html(html);
+  });
+
+  $("a.show_all").click(function(){
+    var show_all_source = $("#show-all-template").html();
+    var show_all_template = Handlebars.compile(show_all_source);
+    var html = show_all_template(animals_data);
+    $('#category').html(html);
+    $('#animals').empty();
   });
 });
 
