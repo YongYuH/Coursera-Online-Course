3c9e9cd92945b84b64426ec57774c8e4648b7c89
YongYuH <larry830326@hotmail.com>
2016-05-06 01:00:49 +0800

    Using handlebar.js template finishing data-driven display

--- Responsive_Web_Design/W4_Hw_Category_of_animals/assignment.js
+++ Responsive_Web_Design/W4_Hw_Category_of_animals/assignment.js
@@ -5,15 +5,25 @@ $(document).ready(function(){
   var html = nav_template(animals_data);
   $("#nav").html(html); 
 
-  var animals_source = $("#animals-template").html();
-  var animals_template = Handlebars.compile(animals_source);
-  var html =animals_template(animals_data);
+  var default_source = $("#default-template").html();
+  var default_template = Handlebars.compile(default_source);
+  var html =default_template(animals_data);
   $('#animals').html(html);
 
-  // $("li.animal_btn").click(function(){
-  //   var html =animals_template(animals_data);
-  //   $('#animals').html(html);
-  // });
+  var animals_source = $("#animals-template").html();
+  var animals_template = Handlebars.compile(animals_source);
+  // var html =animals_template(animals_data);
+  // $('#animals').html(html);
+
+  $("li.animal_btn").click(function(){
+    var category_index = this.id.split("-")[0];
+    var animals_index = this.id.split("-")[1];
+    var specific_category = animals_data.category[category_index];
+    var specific_content = specific_category.animals[animals_index];
+    console.log(specific_category.name);
+    var html = animals_template(specific_category);
+    $('#animals').html(html);
+  });
 });
 
 
